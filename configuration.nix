46b091b71397e34cfe98307c6f8c93a04a3b707c
Salva HG <salva.hg01@gmail.com>
2025-11-26 12:44:26 -0600

    Use fish as default terminal shell

--- configuration.nix
+++ configuration.nix
@@ -79,7 +79,22 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     distrobox
+    fish
   ];
+  
+  environment.sessionVariables = {
+    SSH_AUTH_SOCK = "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock";
+  };
+  
+  programs.bash = {
+  interactiveShellInit = ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    then
+      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    fi
+  '';
+};
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -92,7 +107,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  #services.openssh.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
