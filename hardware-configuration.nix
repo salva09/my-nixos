0ba74a20dbccb524aec0d7cd509ee924170d153a
Salva HG <salva.hg01@gmail.com>
2025-11-27 00:55:22 -0600

    Use zram as swap

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -24,7 +24,7 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  zramSwap.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
