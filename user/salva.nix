92a42f3f72867bc460b8854b2aa5e9937717b610
Salva HG <salva.hg01@gmail.com>
2025-11-29 02:00:40 -0600

    Use fish as default shell

--- user/salva.nix
+++ user/salva.nix
@@ -1,9 +1,13 @@
 { config, pkgs, ... }:
 
 {
+  programs.fish.enable = true;
+  
   users.users.salva = {
     isNormalUser = true;
     description = "Salva";
+    
+    shell = pkgs.fish;
     
     extraGroups = [ 
       "networkmanager"
@@ -50,17 +54,6 @@
       plugins = [
         { name = "tide"; src = pkgs.fishPlugins.tide.src; }
       ];
-    };
-    
-    programs.bash = {
-      enable = true;
-      initExtra = ''
-        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-        then
-          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-        fi
-      '';
     };
     
     programs.git = {
