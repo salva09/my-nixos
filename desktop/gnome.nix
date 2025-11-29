f2d5aab0672156f88886152dfc90487d255e06d0
Salva HG <salva.hg01@gmail.com>
2025-11-29 11:03:21 -0600

    Update gnome desktop

--- desktop/gnome.nix
+++ desktop/gnome.nix
@@ -1,8 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
+  
+  services.displayManager.autoLogin.enable  = true;
+  services.displayManager.autoLogin.user = "salva";
   
   services.gnome.core-apps.enable = true;
   services.gnome.core-developer-tools.enable = false;
@@ -21,6 +24,25 @@
     gnomeExtensions.appindicator
     gnomeExtensions.grand-theft-focus
     gnomeExtensions.advanced-alttab-window-switcher
+  ];
+  
+  programs.dconf.profiles.user.databases = [
+    {
+      lockAll = true; # prevents overriding
+      settings = {
+        "org/gnome/mutter" = {
+          experimental-features = [
+            "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+            "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+            "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+          ];
+          check-alive-timeout = lib.gvariant.mkUint32 10000;
+        };
+        "org/gnome/gnome-session" = {
+          logout-prompt = false;
+        };
+      };
+    }
   ];
   
   programs.kdeconnect = {
