af5fd18f21b876033c760ed47eef61c95b41db86
Salva HG <salva.hg01@gmail.com>
2025-11-29 01:10:04 -0600

    Enable optimization of the nix store

--- default.nix
+++ default.nix
@@ -4,8 +4,19 @@
 
 { config, pkgs, ... }:
 
-{ 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+{
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
   
   networking.networkmanager.enable = true;
 
