08b31620b13adf28b96eab1913f790e7f080a665
Salva HG <salva.hg01@gmail.com>
2025-11-29 12:47:37 -0600

    Simplify vm hardware configuration

--- hosts/vm/hardware.nix
+++ hosts/vm/hardware.nix
@@ -1,40 +1,21 @@
 { config, lib, pkgs, modulesPath, ... }:
 
-{  
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
+{
+  imports = [ 
+    (modulesPath + "/profiles/qemu-guest.nix") 
+  ];
+
+  # We need a root filesystem defined for the build to succeed,
+  # even though build-vm overrides this with its own disk image.
+  fileSystems."/" = { 
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  # Use the systemd-boot loader (generic)
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/47aab21e-761c-4c95-b852-823ba50a93c1";
-      fsType = "ext4";
-    };
-    
-  fileSystems."/mnt/data" =
-    { device = "/dev/disk/by-uuid/8c47ea35-550b-47af-8b51-11007fa79e8a";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5311-F2AA";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  zramSwap.enable = true;
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp10s0.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # Standard platform setting
+  nixpkgs.hostPlatform = "x86_64-linux";
 }
