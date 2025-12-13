857ca50a6fcfc868241c8b80d3b40c8633dd0f89
Salva HG <salva.hg01@gmail.com>
2025-12-13 13:56:59 -0600

    Create cosmic.nix

---
+++ modules/desktop/cosmic.nix
@@ -0,0 +1,12 @@
+{
+  ...
+}:
+
+{
+  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = true;
+
+  services.system76-scheduler.enable = true;
+
+  environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
+}
