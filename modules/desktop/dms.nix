1aeb15d76c443339c9adb6a55b30aa4f3adb1d11
Salva HG <salva.hg01@gmail.com>
2025-12-21 00:13:31 -0600

    Remove unused dms options in new version

--- modules/desktop/dms.nix
+++ modules/desktop/dms.nix
@@ -21,12 +21,9 @@
     enableSystemMonitoring = true; # System monitoring widgets (dgop)
     enableClipboard = true; # Clipboard history manager
     enableVPN = true; # VPN management widget
-    enableBrightnessControl = true; # Backlight/brightness controls
-    enableColorPicker = true; # Color picker tool
     enableDynamicTheming = true; # Wallpaper-based theming (matugen)
     enableAudioWavelength = true; # Audio visualizer (cava)
     enableCalendarEvents = true; # Calendar integration (khal)
-    enableSystemSound = true; # System sound effects
   };
 
   home-manager.sharedModules = [
