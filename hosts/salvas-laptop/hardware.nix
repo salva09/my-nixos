6ecaede6b4645e80c40d0bea41c73f76d30749a3
Salva HG <salva.hg01@gmail.com>
2025-12-28 20:59:47 -0600

    Update salvas-laptop hardware config

--- hosts/salvas-laptop/hardware.nix
+++ hosts/salvas-laptop/hardware.nix
@@ -13,13 +13,40 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/8ce1503d-e616-4750-b165-30c746799232";
-    fsType = "ext4";
-    options = [ "noatime" ];
+    device = "/dev/disk/by-uuid/70cd15fd-453f-42f1-8f88-489cdd5d9200";
+    fsType = "btrfs";
+    options = [
+      "subvol=@"
+      "compress=zstd"
+      "noatime"
+      "discard=async"
+    ];
+  };
+  
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/70cd15fd-453f-42f1-8f88-489cdd5d9200";
+    fsType = "btrfs";
+    options = [
+      "subvol=@nix"
+      "compress=zstd"
+      "noatime"
+      "discard=async"
+    ];
+  };
+  
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/70cd15fd-453f-42f1-8f88-489cdd5d9200";
+    fsType = "btrfs";
+    options = [
+      "subvol=@home"
+      "compress=zstd"
+      "noatime"
+      "discard=async"
+    ];
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/5160-2E14";
+    device = "/dev/disk/by-uuid/C4C6-6D85";
     fsType = "vfat";
     options = [
       "fmask=0077"
