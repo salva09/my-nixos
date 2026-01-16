f43b5b21727e110b3184610706c51fa048bbe991
Salva HG <salva.hg01@gmail.com>
2026-01-16 15:08:07 -0600

    Add essential system packages to cosmic

--- modules/desktop/cosmic.nix
+++ modules/desktop/cosmic.nix
@@ -9,4 +9,13 @@
   services.system76-scheduler.enable = true;
 
   environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.adwaita-mono
+  ];
+
+  environment.systemPackages = with pkgs; [
+    file-roller
+    mission-center
+  ];
 }
