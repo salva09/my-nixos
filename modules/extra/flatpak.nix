1c0df15d5bde711f05f489f8d1acf43cfb04327f
Salva HG <salva.hg01@gmail.com>
2026-03-11 21:29:13 -0600

    Update flatpak module so it only setups flatpak

--- modules/extra/flatpak.nix
+++ modules/extra/flatpak.nix
@@ -1,79 +1,8 @@
 {
-  config,
   inputs,
-  lib,
   ...
 }:
 
-let
-  isDesktop = config.networking.hostName == "salvas-desktop";
-
-  browsers = [
-    "app.zen_browser.zen"
-  ];
-
-  communication = [
-    "com.discordapp.Discord"
-    "org.mozilla.Thunderbird"
-    "io.github.tobagin.karere" # WhatsApp
-    "org.localsend.localsend_app"
-  ];
-
-  productivity = [
-    "org.onlyoffice.desktopeditors"
-    "io.github.pol_rivero.github-desktop-plus"
-    "com.bitwarden.desktop"
-    "io.gitlab.news_flash.NewsFlash"
-  ];
-
-  creative = [
-    "org.blender.Blender"
-    "org.godotengine.Godot"
-    "org.inkscape.Inkscape"
-    "com.icons8.Lunacy"
-    "com.orama_interactive.Pixelorama"
-    "net.pixieditor.PixiEditor"
-    "no.mifi.losslesscut" # Video cutting
-  ];
-
-  media = [
-    "com.dec05eba.gpu_screen_recorder"
-    "info.febvre.Komikku" # Manga reader
-    "com.github.unrud.VideoDownloader"
-  ];
-
-  utilities = [
-    "com.ranfdev.DistroShelf"
-    "com.usebottles.bottles"
-    "org.gnome.Boxes"
-    "de.haeckerfelix.Fragments" # Torrent client
-    "org.fedoraproject.MediaWriter"
-    "ca.desrt.dconf-editor"
-    "io.github.fabrialberio.pinapp"
-    "io.github.alainm23.planify"
-    "com.sidevesh.Luminance"
-  ];
-
-  gaming = [
-    # Launchers
-    # "com.valvesoftware.Steam"
-    "com.heroicgameslauncher.hgl"
-    "org.prismlauncher.PrismLauncher"
-    "org.vinegarhq.Sober" # Roblox
-
-    # Utilities
-    "com.vysp3r.ProtonPlus"
-    "io.github.Foldex.AdwSteamGtk"
-    "io.github.radiolamp.mangojuice"
-    "io.github.wivrn.wivrn" # VR Streaming
-    "org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/24.08"
-  ];
-
-  themes = [
-    "org.gtk.Gtk3theme.adw-gtk3"
-    "org.gtk.Gtk3theme.adw-gtk3-dark"
-  ];
-in
 {
   imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
 
@@ -81,59 +10,21 @@ in
     enable = true;
 
     update.auto = {
-      enable = true;
+      enable = false;
       onCalendar = "daily"; # Default value
     };
 
-    uninstallUnmanaged = true;
+    uninstallUnmanaged = false;
 
-    packages = lib.concatLists [
-      browsers
-      communication
-      productivity
-      creative
-      media
-      utilities
-      gaming
-      themes
+    packages = [
+      "io.github.kolunmi.Bazaar"
     ];
 
     overrides = {
       global = {
         Context.filesystems = [
           "/nix/store:ro"
-          "xdg-config/MangoHud"
-          "xdg-config/gtk-3.0:ro"
-          "xdg-config/gtk-4.0:ro"
-        ]
-        ++ lib.optionals isDesktop [ "/mnt/data" ];
-
-        Environment = {
-          "ELECTRON_OZONE_PLATFORM_HINT" = "auto";
-        };
-      };
-
-      "org.vinegarhq.Sober".Context = {
-        filesystems = [
-          "xdg-run/app/com.discordapp.Discord:create"
-          "xdg-run/discord-ipc-0"
-        ];
-
-        devices = [
-          "input"
-        ];
-      };
-
-      "org.localsend.localsend_app".Context = {
-        filesystems = [
-          "home"
-        ];
-      };
-
-      "com.usebottles.bottles".Context = {
-        filesytems = [
-          "xdg-data/Steam"
-          "~/Games"
+          "/mnt/data"
         ];
       };
     };
