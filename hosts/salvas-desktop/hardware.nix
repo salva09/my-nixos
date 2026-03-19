7cb34da8013b1867351893717263ba6dadec70e8
Salva HG <salva.hg01@gmail.com>
2026-03-18 23:14:57 -0600

    Use zen kernel

--- hosts/salvas-desktop/hardware.nix
+++ hosts/salvas-desktop/hardware.nix
@@ -8,7 +8,7 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
