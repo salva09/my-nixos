2c1819b2b0496cf4f081b8dec01565c5aae5e619
Salva <salva.hg01@gmail.com>
2026-03-23 20:29:40 -0600

    Add kdeconnect and haruna to plasma desktop

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -1,6 +1,15 @@
-{ ... }:
+{
+  pkgs,
+  ...
+}:
 
 {
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  programs.kdeconnect.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    haruna
+  ];
 }
