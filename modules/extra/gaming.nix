7e5b2df4089d630301f56d4fa59c60cd1fd0f796
Salva HG <salva.hg01@gmail.com>
2026-04-14 20:09:20 -0600

    Cleanup gaming module

--- modules/extra/gaming.nix
+++ modules/extra/gaming.nix
@@ -1,24 +1,12 @@
 { pkgs, ... }:
 
 {
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true; # Critical for Steam/Wine (even via Flatpak)
-  };
-
-  hardware.steam-hardware.enable = true;
-
-  services.udev.packages = [
-    pkgs.game-devices-udev-rules
-  ];
-
   programs.steam = {
     enable = true;
 
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    gamescopeSession.enable = false;
 
     extest.enable = true;
 
@@ -49,13 +37,6 @@
       };
     };
   };
-
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true; # Allow it to prioritize itself
-  };
-
-  services.lact.enable = false;
 
   environment.systemPackages = with pkgs; [
     lumafly
