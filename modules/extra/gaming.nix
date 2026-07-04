2d333f015ba1cb8001fedb6137636c71a05f541b
Salva <salva.hg01@gmail.com>
2026-07-04 15:00:03 -0600

    Simplify gaming module

--- modules/extra/gaming.nix
+++ modules/extra/gaming.nix
@@ -1,25 +1,10 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   programs.steam = {
     enable = true;
 
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-
     extest.enable = true;
-
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
-
-    # package = pkgs.steam.override {
-    #   extraArgs = "-steamos3";
-    #   extraEnv = {
-    #     PROTON_ENABLE_WAYLAND = "1";
-    #   };
-    # };
   };
 
   programs.gamemode = {
