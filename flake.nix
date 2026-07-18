29a45db53b8c4b44fbbd951f605a2af99ee35ad4
Salva <salva.hg01@gmail.com>
2026-07-17 22:49:55 -0600

    Remove cachy input

--- flake.nix
+++ flake.nix
@@ -1,39 +1,35 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    cachyos.url = "github:xddxdd/nix-cachyos-kernel";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-test.url = "git+file:///home/salva/Projects/nixpkgs?ref=cosmic-oo7-migration";
   };
 
   outputs =
     inputs@{
       self,
       nixpkgs,
+      nixpkgs-test,
       ...
     }:
     {
       nixosConfigurations = {
-
-        # Desktop Host
         salvas-desktop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = { inherit inputs; };
           modules = [ ./hosts/salvas-desktop ];
         };
 
-        # Laptop Host
         salvas-laptop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = { inherit inputs; };
           modules = [ ./hosts/salvas-laptop ];
         };
 
-        # Your VM Host
-        vm = nixpkgs.lib.nixosSystem {
+        vm = nixpkgs-test.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = { inherit inputs; };
           modules = [ ./hosts/vm ];
         };
-
       };
     };
 }
