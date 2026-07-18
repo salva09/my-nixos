b3fd4bbbeb284b95f1485e251d3b262619e94768
Salva HG <salva.hg@proton.me>
2026-07-18 16:47:11 -0600

    Add flake path to nh

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -54,6 +54,7 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/salva/Projects/my-nixos";
   };
 
   environment.systemPackages = with pkgs; [
